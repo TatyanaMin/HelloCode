54fa6d3145a4fd852b45d7f6e2bf518ff57adcf9
Tanya <108755914+TatyanaMin@users.noreply.github.com>
2022-08-17 09:22:47 +0500

    ass seminar4

---
+++ Seminar4/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+    {
+        static int[] CreateArray(int number)
+        {
+            int[] array = new int[number];
+            for (int i = 0; i < array.Length; i++)
+            {
+                    array[i] = i +1;
+            }
+            return array;
+        }
+           static void WriteArray(int[] array)
+           
+            {
+                    foreach (int number in array)
+                {
+                    Console.Write(number + "  ");   
+                }
+            } 
+                static int[] Shuffle(int[] array)
+                {
+                    for(int i = 0; i < array.Length; i++)
+                    {
+                     int j = new Random().Next(0,i+1);
+                     int number = array[i];  
+                      array[i] = array[j];
+                      array[j] = number;
+                    }
+                    return array;
+                }
+        static void Main(string[] args)
+            {
+                int[] array = CreateArray(10);
+                  WriteArray(array);
+                  Console.WriteLine();
+                  array = Shuffle(array);
+                  WriteArray(Shuffle(array));
+            }
+            
+}
