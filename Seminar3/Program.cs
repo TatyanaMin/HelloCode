0ccb8bc3480af2bd5bdf8a14f280fc93fa55193e
Tanya <108755914+TatyanaMin@users.noreply.github.com>
2022-08-17 09:23:57 +0500

    add seminar3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+    {
+        static void Main(string[] args)
+        {
+            // && - и
+            // || - или
+            int a = 15;
+            int b = 11;
+            int c = 12;
+            int x = 0;
+            bool flag = a > b;
+            int index = flag ? 1 : 2;
+            if((a > b && a > c) || (a == x)) // false  true
+            {
+
+            }
+        }
+    }
+
+
